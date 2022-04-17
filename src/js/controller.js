b41b62200fb558eb0e4dc2416d3bdcbc98562b9e
sohail-load <asohail180@gmail.com>
2022-04-17 20:53:48 +0530

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -120,6 +120,10 @@ const controlAddRecipe = async function (recipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -128,7 +132,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  newFeature();
 };
 
 init();
